44e542ee8b1dd46691315e8ee810261c1ade763a
Tim Birch <171088314+tim-birch@users.noreply.github.com>
2025-12-12 20:17:18 -0700

    minor cleanup

--- Program.cs
+++ Program.cs
@@ -33,24 +33,27 @@ namespace MazeSolver
 		private static int _cellWidth = 0;
 
 		// returns true if RGB of 2 colors match
-		static bool IsMatchingColor(Color color1, Color color2) => color1.R == color2.R && color1.G == color2.G && color1.B == color2.B;
+		private static bool IsMatchingColor(Color color1, Color color2) => color1.R == color2.R && color1.G == color2.G && color1.B == color2.B;
 
 		// converts pixel to 0-based cell number
-		static int PixelToCell(int pixel) => pixel / _cellWidth;
+		private static int PixelToCell(int pixel) => pixel / _cellWidth;
+		
 		// converts 0-based cell number to pixel
-		static int CellToPixel(int cell) => cell * _cellWidth + _cellWidth / 2;
+		private static int CellToPixel(int cell) => cell * _cellWidth + _cellWidth / 2;
 
-		static void DisplayUsage() => Console.WriteLine("USAGE:  MazeSolver <InputImageFile(JPG/PNG/BMP)> <OutputImageFile>");
+		// displays the program usage
+		private static void DisplayUsage() => Console.WriteLine("USAGE:  MazeSolver <InputImageFile(JPG/PNG/BMP)> <OutputImageFile>");
 
-		static void Main(string[] args)
+		// Main program
+		private static void Main(string[] args)
 		{
-			var outFile = string.Empty;
+			string outFile;
 
 			// verify an input image file was passed in, and load it
 			try
 			{
 #if DEBUG
-				var inFile = AppDomain.CurrentDomain.BaseDirectory + @"..\..\Maze2.png";
+				var inFile = AppDomain.CurrentDomain.BaseDirectory + @"..\..\Maze.png";
 				outFile = AppDomain.CurrentDomain.BaseDirectory + @"..\..\Solved.png";
 #else
 				if (args.Length < 2)
@@ -71,10 +74,12 @@ namespace MazeSolver
 				if (File.Exists(outFile))
 					File.Delete(outFile);
 
+				// load the image file into a bitmap
 				_bitmap = new Bitmap(inFile);
 			}
 			catch
 			{
+				// error here if the input file wasn't a valid image file
 				DisplayUsage();
 				return;
 			}
@@ -95,7 +100,7 @@ namespace MazeSolver
 					}
 					if (_cellWidth > 0)
 					{
-						_cellWidth++;
+						_cellWidth++; // add 1 more pixel for the border
 						startX = PixelToCell(x + _cellWidth / 2);
 						startY = PixelToCell(y - _cellWidth / 2);
 						break;
@@ -104,19 +109,21 @@ namespace MazeSolver
 			}
 			if (startX < 0)
 			{
-				Console.WriteLine("ERROR: No starting point was found!");
+				Console.WriteLine("ERROR: No starting point in the maze was found!");
 				return;
 			}
 			Debug.WriteLine($"StartPoint = {startX}/{startY}, Width = {_cellWidth}");
 
-			// allocate matrix to track which cells have been tried
+			// allocate matrix to track the current path while walking through the maze
 			_currentPath = new bool[_bitmap.Width / _cellWidth, _bitmap.Height / _cellWidth];
 
 			using (_graphics = Graphics.FromImage(_bitmap))
 			using (_pathPen = new Pen(_pathColor, 3))
 			{
+				// recursively walk through the maze to see if a solution exists
 				if (SolveMaze(startX, startY, startX, startY))
 				{
+					// save the solution with the path between the start and end colored
 					_bitmap.Save(outFile, ImageFormat.Png);
 					Console.WriteLine($"Solution saved in {outFile}");
 				}
@@ -127,6 +134,8 @@ namespace MazeSolver
 			}
 		}
 
+		// This function is called recursively to find a solution by walking
+		// through the maze one cell at a time and looking in all directions.
 		static bool SolveMaze(int fromX, int fromY, int toX, int toY)
 		{
 			// first check if out-of-bounds
@@ -188,7 +197,7 @@ namespace MazeSolver
 			return false;
 		}
 
-		// return true if a border exists between cells that are side by side
+		// returns true if a border exists between cells that are side by side
 		static bool HasBorderBetweenXCells(int fromX, int toX, int y)
 		{
 			// check every pixel on the X-axis between the cells
@@ -201,7 +210,7 @@ namespace MazeSolver
 			return false;
 		}
 		
-		// return true if a border exists between cells that are above/below each other
+		// returns true if a border exists between cells that are above/below each other
 		static bool HasBorderBetweenYCells(int fromY, int toY, int x)
 		{
 			// check every pixel on the Y-axis between the cells
